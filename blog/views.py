de6eb436ec86162329070c45013503ae327283e4
unknown <mohammedabakrim@gmail.com>
2023-12-08 18:38:26 +0100

    adding simple templates

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-from django.http import HttpResponse
+
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html')
